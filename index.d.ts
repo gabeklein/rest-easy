18c751c14eee3a228afa95547a4716307654aedb
Gabe <gabe@termtm.com>
2019-10-20 11:15:56 -0400

    fix incomplete definitions

--- index.d.ts
+++ index.d.ts
@@ -1,70 +1,72 @@
-import express, { RequestHandler } from 'express';
+import express, { RequestHandler, Express } from 'express';
+import { ApplicationRequestHandler } from 'express-serve-static-core';
 
-declare namespace REST {
-
-  interface Request<T> 
-    extends express.Request {
-    query: T
-  }
-
-  interface RequestWithBody<T> 
-    extends express.Request {
-    body: T
-  }
-
-  type Handle<I, O = void> = (
-    req: Request<I>,
-    res: express.Response
-  ) => O | Promise<O>;
-
-  type HandleBody<I, O = void> = (
-    req: RequestWithBody<I>,
-    res: express.Response
-  ) => O | Promise<O>;
-
-  /**
-   * Register a GET request with your exported App.
-   * 
-   * @param loc - Resource url
-   * @param fn - Handler body
-   */
-  function GET<T extends Object = any, O = any>
-    (loc: string, fn: Handle<T, O>): void;
-
-  /**
-   * Register a POST request with your exported App.
-   * 
-   * @param loc - Resource url
-   * @param fn - Handler body
-   */
-  function POST<T extends Object = any, O = any>
-    (loc: string, fn: HandleBody<T, O>): void;
-
-  /**
-   * Register a PUT request with your exported App.
-   * 
-   * @param loc - Resource url
-   * @param fn - Handler body
-   */
-  function PUT<T extends Object = any, O = any>
-    (loc: string, fn: HandleBody<T, O>): void;
-
-  /**
-   * Register a PATCH request with your exported App.
-   * 
-   * @param loc - Resource url
-   * @param fn - Handler body
-   */
-  function PATCH<T extends Object = any, O = any>
-    (loc: string, fn: HandleBody<T, O>): void;
-
-  /**
-   * Register a DELETE request with your exported App.
-   * 
-   * @param loc - Resource url
-   * @param fn - Handler body
-   */
-  function DELETE<T extends Object = any, O = any>
-    (loc: string, fn: Handle<T, O>): void;
+interface Request<T> 
+  extends express.Request {
+  query: T
 }
 
+interface RequestWithBody<T> 
+  extends express.Request {
+  body: T
+}
+
+type Handle<I, O = void> = (
+  req: Request<I>,
+  res: express.Response
+) => O | Promise<O>;
+
+type HandleBody<I, O = void> = (
+  req: RequestWithBody<I>,
+  res: express.Response
+) => O | Promise<O>;
+
+/**
+ * Register a GET request with your exported App.
+ * 
+ * @param loc - Resource url
+ * @param fn - Handler body
+ */
+export function GET<T extends Object = any, O = any>
+  (loc: string, fn: Handle<T, O>): void;
+
+/**
+ * Register a POST request with your exported App.
+ * 
+ * @param loc - Resource url
+ * @param fn - Handler body
+ */
+export function POST<T extends Object = any, O = any>
+  (loc: string, fn: HandleBody<T, O>): void;
+
+/**
+ * Register a PUT request with your exported App.
+ * 
+ * @param loc - Resource url
+ * @param fn - Handler body
+ */
+export function PUT<T extends Object = any, O = any>
+  (loc: string, fn: HandleBody<T, O>): void;
+
+/**
+ * Register a PATCH request with your exported App.
+ * 
+ * @param loc - Resource url
+ * @param fn - Handler body
+ */
+export function PATCH<T extends Object = any, O = any>
+  (loc: string, fn: HandleBody<T, O>): void;
+
+/**
+ * Register a DELETE request with your exported App.
+ * 
+ * @param loc - Resource url
+ * @param fn - Handler body
+ */
+export function DELETE<T extends Object = any, O = any>
+  (loc: string, fn: Handle<T, O>): void;
+
+export const USE: ApplicationRequestHandler<Express>; 
+
+declare const server: Express;
+export { server, server as default }
