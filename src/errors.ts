e1c2e82bdc980b57041bcce7fbce7bd3feb8054f
Gabe <gabe@termtm.com>
2019-10-24 23:44:00 -0400

    remove jsdocs from source code

--- src/errors.ts
+++ src/errors.ts
@@ -8,27 +8,22 @@ export class RestError extends Error {
   }
 }
 
-/** Throw this to send `404 (Not Found)` with `message` to client. */
 export function Forbid(message?: string, error?: string) {
   return new RestError(403, message, error)
 }
 
-/** Throw this to send `404 (Not Found)` with `message` to client. */
 export function NotFound(message?: string, error?: string) {
   return new RestError(404, message, error)
 }
 
-/** Throw this to send `500 (Interal Server Error)` with `message` to client. */
 export function Internal(message?: string, error?: string) {
   return new RestError(500, message, error)
 }
 
-/** Throw this to send `400 (Bad Input)` with `message` to client. */
 export function BadInput(message?: string, error?: string) {
   return new RestError(400, message, error)
 }
 
-/** Throw this to send any http status with `message` to client. */
 export function Code(status: number, message?: string, error?: string) {
   return new RestError(status, message, error)
 }
